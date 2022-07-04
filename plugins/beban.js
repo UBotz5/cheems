434f2688fede89e4c1187f37daea59e45a4df860
UBotz5 <107531148+UBotz5@users.noreply.github.com>
2022-07-04 20:56:41 +0700

    Add files via upload

--- plugins/beban.js
+++ plugins/beban.js
@@ -1,1 +1,21 @@
-jsjs
+let handler = async (m, { conn }) => {
+let caption = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+ |  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`
+
+conn.sendBut( m.chat, caption, ``, `OKE KAK><`,  m)
+
+       }
+       
+XeonCheems3.customPrefix = /^(intro)/i
+XeonCheems3.command = new RegExp
+module.exports = XeonCheems3
